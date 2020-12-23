497e9aa5235bec41c9c2b427e64318302f33824f
root <root@kali.Domain>
2020-12-23 04:23:19 +0000

    TREADSTONE

--- install.py
+++ install.py
@@ -73,6 +73,7 @@ os.system("echo '" + Reset + "'")
 # -------------------------------------------------------------------------------------
 
 dirList = ["ROGUEAGENT", "TREADSTONE", "BLACKBRIAR", "OUTCOME", "LARX"]
+
 for x in range(0, len(dirList)):
    if not os.path.exists(dirList[x]):
       os.system("mkdir " + dirList[x])
@@ -86,7 +87,6 @@ for x in range(0, len(dirList)):
 # -------------------------------------------------------------------------------------
 
 print("\n\n[*] Creating program banners, please wait...")
-
 os.chdir("ROGUEAGENT")
 
 with open("banner1.txt","w") as banner:
@@ -162,22 +162,24 @@ os.chdir("..")
 print("[*] Installing system requirements, please wait...")
 
 list1 = ["hashcat", "python3-pip", "python3-ldap", "gobuster", "crackmapexec", "exiftool", "rlwrap", "xdotool", "seclists"]
+
 for x in range(0, len(list1)):
-   os.system("apt-get install " + list1[x] + " -y >> log.tmp 2>&1")
+   os.system("apt-get install " + list1[x] + " -y >> log.txt 2>&1")
 
 list2 = ["bloodhound", "kerbrute", "smtp-user-enum", "termcolor", "adidnsdump"]
-for x in range(0, len(list2)):
-  os.system("pip3 install " + list2[x] + " >> log.tmp 2>&1")
 
-os.system("python3 -m pip install 'neo4j-driver==1.7.0' --force-reinstall >> log.tmp 2>&1")
-os.system("python3 -m pip install 'neo4j==1.7.0' --force-reinstall >> log.tmp 2>&1")
-os.system("python3 -m pip install -U ldap3 >> log.tmp 2>&1")
-os.system("python3 -m pip install aclpwn >> log.tmp 2>&1")
-os.system("gem install evil-winrm >> log.tmp 2>&1")
+for x in range(0, len(list2)):
+  os.system("pip3 install " + list2[x] + " >> log.txt 2>&1")
+
+os.system("python3 -m pip install 'neo4j-driver==1.7.0' --force-reinstall >> log.txt 2>&1")
+os.system("python3 -m pip install 'neo4j==1.7.0' --force-reinstall >> log.txt 2>&1")
+os.system("python3 -m pip install -U ldap3 >> log.txt 2>&1")
+os.system("python3 -m pip install aclpwn >> log.txt 2>&1")
+os.system("gem install evil-winrm >> log.txt 2>&1")
 
 if not os.path.exists("/usr/share/doc/python3-impacket/examples/windapsearch.py"):
-   os.system("git clone https://github.com/ropnop/windapsearch.git >> log.tmp 2>&1")
-   os.system("mv windapsearch/windapsearch.py /usr/share/doc/python3-impacket/examples/windapsearch.py >> log.tmp 2>&1")
+   os.system("git clone https://github.com/ropnop/windapsearch.git >> log.txt 2>&1")
+   os.system("mv windapsearch/windapsearch.py /usr/share/doc/python3-impacket/examples/windapsearch.py >> log.txt 2>&1")
    shutil.rmtree("windapsearch")
 
 # -------------------------------------------------------------------------------------
@@ -189,73 +191,87 @@ if not os.path.exists("/usr/share/doc/python3-impacket/examples/windapsearch.py"
 # -------------------------------------------------------------------------------------
 
 print("[*] Installing windows and linux exploits, please wait...")   
+
 os.chdir("TREADSTONE")
 
-os.system("wget https://download.sysinternals.com/files/Procdump.zip -O Procdump.zip > log.tmp 2>&1")
-os.system("unzip Procdump.zip >> log.tmp 2>&1")
+os.system("wget https://download.sysinternals.com/files/Procdump.zip -O Procdump.zip > log.txt 2>&1")
+os.system("unzip Procdump.zip >> log.txt 2>&1")
 if os.path.exists("Procdump.zip"):
    os.remove("Procdump.zip")
 os.remove("Eula.txt")
 os.remove("procdump64a.exe")
-os.system("wget 'https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/raw/master/winPEAS/winPEASexe/winPEAS/bin/Obfuscated%20Releases/winPEASx64.exe' -O winpeas64.exe >> log.tmp 2>&1")
-os.system("wget 'https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/raw/master/winPEAS/winPEASexe/winPEAS/bin/Obfuscated%20Releases/winPEASx86.exe' -O winpeas32.exe >> log.tmp 2>&1")
-os.system("wget 'https://github.com/BloodHoundAD/BloodHound/raw/master/Collectors/SharpHound.exe' -O sharphound.exe >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1' -O ../LARX/sharphound.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/AzureHound.ps1' -O ../LARX/azurehound.ps1 >> log.tmp 2>&1")
-os.system("git clone https://github.com/TsukiCTF/Lovely-Potato.git >> log.tmp 2>&1")
-os.system("mv ./Lovely-Potato/Invoke-LovelyPotato.ps1 ../LARX/lovelypotato.ps1 >> log.tmp 2>&1")
-os.system("mv ./Lovely-Potato/JuicyPotato-Static.exe ./juicypotato.exe >> log.tmp 2>&1")
-os.system("mv ./Lovely-Potato/test_clsid.bat ./ >> log.tmp 2>&1")
+
+os.system("wget 'https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/raw/master/winPEAS/winPEASexe/winPEAS/bin/Obfuscated%20Releases/winPEASx64.exe' -O winpeas64.exe >> log.txt 2>&1")
+os.system("wget 'https://github.com/carlospolop/privilege-escalation-awesome-scripts-suite/raw/master/winPEAS/winPEASexe/winPEAS/bin/Obfuscated%20Releases/winPEASx86.exe' -O winpeas32.exe >> log.txt 2>&1")
+
+os.system("wget 'https://github.com/BloodHoundAD/BloodHound/raw/master/Collectors/SharpHound.exe' -O sharphound.exe >> log.txt 2>&1")
+os.system("wget 'https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/SharpHound.ps1' -O ../LARX/sharphound.ps1 >> log.txt 2>&1")
+os.system("wget 'https://raw.githubusercontent.com/BloodHoundAD/BloodHound/master/Collectors/AzureHound.ps1' -O ../LARX/azurehound.ps1 >> log.txt 2>&1")
+
+os.system("git clone https://github.com/TsukiCTF/Lovely-Potato.git >> log.txt 2>&1")
+os.system("mv ./Lovely-Potato/Invoke-LovelyPotato.ps1 ../LARX/lovelypotato.ps1 >> log.txt 2>&1")
+os.system("mv ./Lovely-Potato/JuicyPotato-Static.exe ./juicypotato.exe >> log.txt 2>&1")
+os.system("mv ./Lovely-Potato/test_clsid.bat ./ >> log.txt 2>&1")
 os.remove("./Lovely-Potato/README.md")
 shutil.rmtree("Lovely-Potato")
-os.system("wget 'https://github.com/antonioCoco/RoguePotato/releases/download/1.0/RoguePotato.zip' -O RoguePotato.zip >> log.tmp 2>&1")
-os.system("unzip RoguePotato.zip >> log.tmp 2>&1")
+os.system("wget 'https://github.com/antonioCoco/RoguePotato/releases/download/1.0/RoguePotato.zip' -O RoguePotato.zip >> log.txt 2>&1")
+os.system("unzip RoguePotato.zip >> log.txt 2>&1")
 if os.path.exists("RoguePotato.zip"):
    os.remove("RoguePotato.zip")
-os.system("mv ./RogueOxidResolver.exe ./rogueoxidresolver.exe >> log.tmp 2>&1")
-os.system("mv ./RoguePotato.exe ./roguepotato.exe >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/fox-it/Invoke-ACLPwn/master/Invoke-ACLPwn.ps1' -O ../LARX/aclpwn.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/411Hall/JAWS/raw/master/jaws-enum.ps1' -O ../LARX/jawsenum.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/besimorhino/powercat/raw/master/powercat.ps1' -O ../LARX/powercat.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/HarmJ0y/PowerUp/raw/master/PowerUp.ps1' -O ../LARX/powerup.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/S3cur3Th1sSh1t/WinPwn/raw/master/WinPwn.ps1' -O ../LARX/winpwn.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/PowerShellMafia/PowerSploit/raw/master/Exfiltration/Out-Minidump.ps1' -O ../LARX/mimidump.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/PowerShellMafia/PowerSploit/raw/master/Exfiltration/Invoke-Mimikatz.ps1' -O ../LARX/mimikatz.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/PowerShellMafia/PowerSploit/raw/master/Recon/PowerView.ps1' -O ../LARX/powerview.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/Kevin-Robertson/Powermad/raw/master/Powermad.ps1' -O ../LARX/powermad.ps1 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/jpillora/chisel/releases/download/v1.7.2/chisel_1.7.2_windows_amd64.gz' -O chisel.gz >> log.tmp 2>&1")
-os.system("gunzip chisel.gz")
-os.system("mv chisel win_chisel64.exe >> log.tmp 2>&1")
-os.system("wget 'https://github.com/jpillora/chisel/releases/download/v1.7.2/chisel_1.7.2_windows_386.gz' -O chisel.gz >> log.tmp 2>&1")
-os.system("gunzip chisel.gz")
-os.system("mv chisel win_chisel32.exe >> log.tmp 2>&1")
-os.system("wget 'https://github.com/jpillora/chisel/releases/download/v1.7.3/chisel_1.7.3_linux_amd64.gz' -O chisel.gz >> log.tmp 2>&1")
-os.system("gunzip chisel.gz")
-os.system("mv chisel lin_chisel64 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/jpillora/chisel/releases/download/v1.7.3/chisel_1.7.3_linux_386.gz' -O chisel.gz >> log.tmp 2>&1")
-os.system("gunzip chisel.gz")
-os.system("mv chisel lin_chisel32 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe' -O rubeus.exe >> log.tmp 2>&1")
-os.system("wget 'https://nmap.org/dist/nmap-7.80-setup.exe' -O nmapsetup.exe >> log.tmp 2>&1")
-os.system("cp /usr/share/windows-resources/mimikatz/Win32/mimikatz.exe ./mimikatz32.exe >> log.tmp 2>&1")
-os.system("cp /usr/share/windows-resources/mimikatz/x64/mimikatz.exe ./mimikatz64.exe >> log.tmp 2>&1")
-os.system("cp /usr/share/windows-resources/binaries/nc.exe nc64.exe >> log.tmp 2>&1")
-os.system("cp /usr/share/windows-resources/binaries/plink.exe plink64.exe >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/WhiteWinterWolf/wwwolf-php-webshell/master/webshell.php' -O webshell.php >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php' -O myshell.php >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh' -O linenum.sh >> log.tmp 2>&1")
-os.system("wget 'https://github.com/diego-treitos/linux-smart-enumeration/raw/master/lse.sh' -O linenumplus.sh >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/Adlemann/linPE/master/linpe.sh' -O linpe.sh >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh' -O linpeas.sh >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/Arr0way/linux-local-enumeration-script/master/linux-local-enum.sh' -O coffee.sh >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/WhiteWinterWolf/wwwolf-php-webshell/master/webshell.php' -O webshell.php >> log.tmp 2>&1")
-os.system("wget 'https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32' -O pspy32 >> log.tmp 2>&1")
-os.system("wget 'https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64' -O pspy64 >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/kkamagui/linux-kernel-exploits/master/kernel-4.4.0-31-generic/CVE-2016-5195/compile.sh' -O naughtycowcompile.sh >> log.tmp 2>&1")
-os.system("wget 'https://raw.githubusercontent.com/kkamagui/linux-kernel-exploits/master/kernel-4.4.0-31-generic/CVE-2016-5195/naughtyc0w.c' -O naughthycow.c >> log.tmp 2>&1")
-os.system("chmod +X *.*")
+os.system("mv ./RogueOxidResolver.exe ./rogueoxidresolver.exe >> log.txt 2>&1")
+os.system("mv ./RoguePotato.exe ./roguepotato.exe >> log.txt 2>&1")
 
+os.system("wget 'https://raw.githubusercontent.com/fox-it/Invoke-ACLPwn/master/Invoke-ACLPwn.ps1' -O ../LARX/aclpwn.ps1 >> log.txt 2>&1")
+os.system("wget 'https://github.com/411Hall/JAWS/raw/master/jaws-enum.ps1' -O ../LARX/jawsenum.ps1 >> log.txt 2>&1")
+os.system("wget 'https://github.com/besimorhino/powercat/raw/master/powercat.ps1' -O ../LARX/powercat.ps1 >> log.txt 2>&1")
+os.system("wget 'https://github.com/HarmJ0y/PowerUp/raw/master/PowerUp.ps1' -O ../LARX/powerup.ps1 >> log.txt 2>&1")
+os.system("wget 'https://github.com/S3cur3Th1sSh1t/WinPwn/raw/master/WinPwn.ps1' -O ../LARX/winpwn.ps1 >> log.txt 2>&1")
+os.system("wget 'https://github.com/PowerShellMafia/PowerSploit/raw/master/Exfiltration/Out-Minidump.ps1' -O ../LARX/mimidump.ps1 >> log.txt 2>&1")
+os.system("wget 'https://github.com/PowerShellMafia/PowerSploit/raw/master/Exfiltration/Invoke-Mimikatz.ps1' -O ../LARX/mimikatz.ps1 >> log.txt 2>&1")
+os.system("wget 'https://github.com/PowerShellMafia/PowerSploit/raw/master/Recon/PowerView.ps1' -O ../LARX/powerview.ps1 >> log.txt 2>&1")
+os.system("wget 'https://github.com/Kevin-Robertson/Powermad/raw/master/Powermad.ps1' -O ../LARX/powermad.ps1 >> log.txt 2>&1")
+
+os.system("wget 'https://github.com/jpillora/chisel/releases/download/v1.7.2/chisel_1.7.2_windows_amd64.gz' -O chisel.gz >> log.txt 2>&1")
+os.system("gunzip chisel.gz")
+os.system("mv chisel win_chisel64.exe >> log.txt 2>&1")
+os.system("wget 'https://github.com/jpillora/chisel/releases/download/v1.7.2/chisel_1.7.2_windows_386.gz' -O chisel.gz >> log.txt 2>&1")
+os.system("gunzip chisel.gz")
+os.system("mv chisel win_chisel32.exe >> log.txt 2>&1")
+os.system("wget 'https://github.com/jpillora/chisel/releases/download/v1.7.3/chisel_1.7.3_linux_amd64.gz' -O chisel.gz >> log.txt 2>&1")
+os.system("gunzip chisel.gz")
+os.system("mv chisel lin_chisel64 >> log.txt 2>&1")
+os.system("wget 'https://github.com/jpillora/chisel/releases/download/v1.7.3/chisel_1.7.3_linux_386.gz' -O chisel.gz >> log.txt 2>&1")
+os.system("gunzip chisel.gz")
+os.system("mv chisel lin_chisel32 >> log.txt 2>&1")
+
+os.system("wget 'https://github.com/r3motecontrol/Ghostpack-CompiledBinaries/raw/master/Rubeus.exe' -O rubeus.exe >> log.txt 2>&1")
+os.system("wget 'https://nmap.org/dist/nmap-7.80-setup.exe' -O nmapsetup.exe >> log.txt 2>&1")
+
+os.system("cp /usr/share/windows-resources/mimikatz/Win32/mimikatz.exe ./mimikatz32.exe >> log.txt 2>&1")
+os.system("cp /usr/share/windows-resources/mimikatz/x64/mimikatz.exe ./mimikatz64.exe >> log.txt 2>&1")
+
+os.system("cp /usr/share/windows-resources/binaries/nc.exe nc64.exe >> log.txt 2>&1")
+os.system("cp /usr/share/windows-resources/binaries/plink.exe plink64.exe >> log.txt 2>&1")
+
+os.system("wget 'https://raw.githubusercontent.com/WhiteWinterWolf/wwwolf-php-webshell/master/webshell.php' -O webshell.php >> log.txt 2>&1")
+os.system("wget 'https://raw.githubusercontent.com/pentestmonkey/php-reverse-shell/master/php-reverse-shell.php' -O myshell.php >> log.txt 2>&1")
+
+os.system("wget 'https://raw.githubusercontent.com/rebootuser/LinEnum/master/LinEnum.sh' -O linenum.sh >> log.txt 2>&1")
+os.system("wget 'https://github.com/diego-treitos/linux-smart-enumeration/raw/master/lse.sh' -O linenumplus.sh >> log.txt 2>&1")
+os.system("wget 'https://raw.githubusercontent.com/Adlemann/linPE/master/linpe.sh' -O linpe.sh >> log.txt 2>&1")
+os.system("wget 'https://raw.githubusercontent.com/carlospolop/privilege-escalation-awesome-scripts-suite/master/linPEAS/linpeas.sh' -O linpeas.sh >> log.txt 2>&1")
+os.system("wget 'https://raw.githubusercontent.com/Arr0way/linux-local-enumeration-script/master/linux-local-enum.sh' -O coffee.sh >> log.txt 2>&1")
+os.system("wget 'https://raw.githubusercontent.com/WhiteWinterWolf/wwwolf-php-webshell/master/webshell.php' -O webshell.php >> log.txt 2>&1")
+
+os.system("wget 'https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy32' -O pspy32 >> log.txt 2>&1")
+os.system("wget 'https://github.com/DominicBreuker/pspy/releases/download/v1.2.0/pspy64' -O pspy64 >> log.txt 2>&1")
+
+os.system("wget 'https://raw.githubusercontent.com/kkamagui/linux-kernel-exploits/master/kernel-4.4.0-31-generic/CVE-2016-5195/compile.sh' -O naughtycowcompile.sh >> log.txt 2>&1")
+os.system("wget 'https://raw.githubusercontent.com/kkamagui/linux-kernel-exploits/master/kernel-4.4.0-31-generic/CVE-2016-5195/naughtyc0w.c' -O naughthycow.c >> log.txt 2>&1")
+
+os.system("chmod +X *.*")
 os.chdir("..")
+
 os.chdir("LARX")
 os.system("sed -i -e '/<#/,/#>/c\\' *.ps1")
 os.system("sed -i -e 's/^[[:space:]]*#.*$//g' *.ps1")
@@ -271,7 +287,9 @@ os.chdir ("..")
 
 print("[*] Installing exploit obfuscation software, please wait...")
 os.chdir("OUTCOME")
-os.system("git clone https://github.com/phra/PEzor.git >> log.tmp 2>&1")
+
+os.system("git clone https://github.com/phra/PEzor.git >> log.txt 2>&1")
+
 os.chdir("PEzor")
 os.system("bash install.sh >> keepme.txt 2>&1")
 
@@ -282,5 +300,6 @@ os.system(last_line)
 
 os.chdir("..")
 os.chdir("..")
+
 print("[*] All done!!...")
 #EoF
