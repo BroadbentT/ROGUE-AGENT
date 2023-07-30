9a9a0e5e08bdd3e7bacf4c9ed9545a5463a17f71
root <root@kali.Domain>
2023-07-30 19:37:10 +0100

    LARX

--- README.md
+++ README.md
@@ -33,7 +33,7 @@ A comprehensive, yet sleek and portable python script file that enables cyber se
 | python3   | exploits.py           | 7e1381abca8256af1392daeb68524746 | Install Exploits    | TREADSTONE   |
 | python3   | RogueAgent.py         | 1842c3c57a3038f6a546283191493f16 | Command Centre      | TREADSTONE   |
 | python3   | Dark-Operative.py     | ac6df9f3f4e7c61640b19a5feeca50d3 | Command Centre (WS) | LARX (BETA)  |
-| python3   | shell.py              | bbbcd051d5fac1351a268c3f910e48d0 | Auto Reverse Shell  | LARX         |
+| python3   | shell.py              | 0ae617318d2c78a587ea2844733d1db0 | Auto Reverse Shell  | LARX         |
 | SQLite3   | RA.db                 | bcfd3cd2c5846250f9614c4c6b71b0b3 | Program Database    | TREADSTONE   |
        
 ## CONSOLE DISPLAY AND VIDEO LINK
--- shell.py
+++ shell.py
@@ -39,12 +39,22 @@ if os.geteuid() != 0:
    print("\nPlease run this python script as root...")
    exit(True)
 
+if len(sys.argv) < 2:
+   port = "1234"
+else:
+   port = sys.argv[1]  
+   
+# -------------------------------------------------------------------------------------
+# AUTHOR  : Terence Broadbent                                                    
+# CONTRACT: GitHub                                                               
+# Version : 1.0                                                               
+# Details : Create listener.
+# Modified: N/A                                                               
+# -------------------------------------------------------------------------------------   
+
 os.system("clear")
 os.system("xdotool key Alt+Shift+S; xdotool type 'REVERSE SHELL'; xdotool key Return")
 os.system("pyfiglet 'REVERSE SHELL'")
-    
-# -------------------------------------------------------------------------------------
-
 print (colored("USE THE FOLLOWING COMMANDS TO STABALISE THE SHELL IF NECESSARY..\n",colour3))
 print (colored("python3 -c 'import pty; pty.spawn(\"/bin/bash\")'", colour1))
 print (colored("Ctrl Z", colour1))
@@ -52,5 +62,5 @@ print (colored("stty raw -echo", colour1))
 print (colored("fg", colour1))
 print (colored("export TERM=xterm\n", colour1))
 
-os.system("rlwrap nc -nvlp 1234")
+os.system("rlwrap nc -nvlp " + port + " ")
 
